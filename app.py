e83fe24d0a4476a187d1a99a5ad6f2bce2928055
Chingiz Tursunbayev <81404788+ChingizIbnVaxob@users.noreply.github.com>
2024-04-25 19:01:58 +0900

    Project files have been uploaded.

---
+++ app.py
@@ -0,0 +1,38 @@
+import streamlit as st
+from fastai.vision.all import *
+import plotly.express as px
+import pathlib
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+
+# Title
+st.title("Transportni klassifikatsiya qiluvchi model")
+
+# Upload image
+file = st.file_uploader("Rasm yuklash", type=['png', 'jpeg', 'img'])
+
+# Load model
+model = load_learner(str("transport_model.pkl"))
+
+# Prediction
+if file is not None:
+    # Create PIL image
+    img = PILImage.create(file)
+
+    # Display uploaded image
+    st.image(img, caption='Uploaded Image', use_column_width=True)
+
+    # Perform prediction
+    pred, pred_id, probs = model.predict(img)
+
+    # Display prediction result
+    st.success(pred)
+
+    #Display prediction accuracy
+    st.info(f'Accuracy: {probs[pred_id]*100:.1f}')
+
+    #display figure
+    fig = px.bar(x=probs*100, y=model.dls.vocab)
+    st.plotly_chart(fig)
+
+    C:\Users\INHA\Desktop\PYTHON_SCRIPTS\transports\app.py
